f5d500bdd710c16199be5654d9461a5706c2bcd8
AlexMaderaP <alejandro.maderap@gmail.com>
2024-02-21 14:08:57 -0600

    Init Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>First Typescript App</h1>
+      <p>Schedule Craft</p>
+    </>
+  );
+}
+
+export default App;
