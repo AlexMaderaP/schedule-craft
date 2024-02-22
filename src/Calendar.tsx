efccb6fa5344089dddb4deb06025276e2e3e871e
AlexMaderaP <alejandro.maderap@gmail.com>
2024-02-22 11:22:53 -0600

    Add Calendar

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -1,4 +1,21 @@
+import {
+  eachDayOfInterval,
+  endOfMonth,
+  endOfWeek,
+  format,
+  startOfMonth,
+  startOfWeek,
+} from "date-fns";
+import { useState } from "react";
+
 function Calendar() {
+  const [monthShowed, setMonthShowed] = useState(new Date());
+
+  const dateRange = eachDayOfInterval({
+    start: startOfWeek(startOfMonth(monthShowed)),
+    end: endOfWeek(endOfMonth(monthShowed)),
+  });
+
   return (
     <div className="calendar">
       <div className="header">
@@ -7,7 +24,16 @@ function Calendar() {
           <button className="month-change-btn">&lt;</button>
           <button className="month-change-btn">&gt;</button>
         </div>
-        <span className="month-title">June 2023</span>
+        <span className="month-title">{format(monthShowed, "MMMM yyyy")}</span>
+      </div>
+      <div className="days">
+        {dateRange.map((day) => (
+          <div className="day ">
+            <div className="day-header">
+              <div className="day-number">{format(day, "d")}</div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
