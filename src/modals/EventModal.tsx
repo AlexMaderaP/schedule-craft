f6f9d454bdfd59e7f9e547ff4e57911c8e8450ab
AlexMaderaP <alejandro.maderap@gmail.com>
2024-02-28 17:04:49 -0600

    Add closing animation

--- src/modals/EventModal.tsx
+++ src/modals/EventModal.tsx
@@ -2,6 +2,7 @@ import { createPortal } from "react-dom";
 import AddEventForm from "../components/AddEventForm";
 import { EventType } from "../App";
 import EditEventForm from "../components/EditEventForm";
+import { useLayoutEffect, useRef, useState } from "react";
 
 type EventModalProps = {
   openEventModal: boolean;
@@ -18,25 +19,42 @@ function EventModal({
   event,
   clearEvent,
 }: EventModalProps) {
+  const [isClosing, setIsClosing] = useState(false);
+  const prevIsOpen = useRef<boolean>();
+
   const isEditing = event != undefined;
 
+  useLayoutEffect(() => {
+    if (!openEventModal && prevIsOpen.current) {
+      setIsClosing(true);
+    }
+
+    prevIsOpen.current = openEventModal;
+  }, [openEventModal]);
+
+  if (!openEventModal && !isClosing) return null;
+
   return createPortal(
-    <div className={`modal-overlay ${openEventModal ? "show" : ""}`}>
-      <div className="modal">
-        <div className="modal-body">
-          {isEditing ? (
-            <EditEventForm
-              event={event}
-              closeEventModal={closeEventModal}
-              clearEvent={clearEvent}
-            />
-          ) : (
-            <AddEventForm
-              dateToCreateEvent={dateToCreateEvent}
-              closeEventModal={closeEventModal}
-            />
-          )}
-        </div>
+    <div
+      onAnimationEnd={() => {
+        setIsClosing(false);
+      }}
+      className={`modal ${isClosing ? "closing" : ""}`}
+    >
+      <div className="overlay"></div>
+      <div className="modal-body">
+        {isEditing ? (
+          <EditEventForm
+            event={event}
+            closeEventModal={closeEventModal}
+            clearEvent={clearEvent}
+          />
+        ) : (
+          <AddEventForm
+            dateToCreateEvent={dateToCreateEvent}
+            closeEventModal={closeEventModal}
+          />
+        )}
       </div>
     </div>,
     document.querySelector("#modals")!
