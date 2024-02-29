e02f4869c33b30e6bf966c042ccdc36d173ef232
AlexMaderaP <alejandro.maderap@gmail.com>
2024-02-29 13:32:08 -0600

    Add view more button for event overflow

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import { EventType } from "../App";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 type EventsProps = {
   events: EventType[];
@@ -8,17 +9,52 @@ type EventsProps = {
 };
 
 function Events({ events, setOpenEditEvent, setEventToEdit }: EventsProps) {
+  const myEvents = useRef<HTMLDivElement>(null);
+  const [visibleEvents, setVisibleEvents] = useState(events.length);
+
   events.sort(compareDates);
+  const eventsToShow = events.slice(0, visibleEvents);
+  const EventsHidden = events.length - visibleEvents;
 
   function handleEditEvent(event: EventType) {
     setOpenEditEvent();
     setEventToEdit(event);
   }
 
+  useLayoutEffect(() => {
+    if (!myEvents.current) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        let count = 0;
+
+        for (const entry of entries) {
+          if (entry.isIntersecting) {
+            count++;
+          }
+        }
+
+        setVisibleEvents(count);
+      },
+      { threshold: 1 }
+    );
+
+    myEvents.current.querySelectorAll(".event").forEach((event) => {
+      observer.observe(event);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(visibleEvents);
+  }, [visibleEvents]);
+
+  //Check If everything is displaying
+  //If Something is not displaying don't render it, instead render button view more
+
   return (
     <>
-      <div className="events">
-        {events.map((event) => (
+      <div ref={myEvents} className="events">
+        {eventsToShow.map((event) => (
           <button
             key={event.id}
             onClick={() => handleEditEvent(event)}
@@ -35,6 +71,9 @@ function Events({ events, setOpenEditEvent, setEventToEdit }: EventsProps) {
             <div className="event-name">{event.name}</div>
           </button>
         ))}
+        {EventsHidden > 0 && (
+          <button className="events-view-more-btn">+{EventsHidden} More</button>
+        )}
       </div>
     </>
   );
